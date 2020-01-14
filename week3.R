44483dd23635599a0f3862c5bc56b10ee1ebf7f1
d73mwf <34099039+DomDF@users.noreply.github.com>
2020-01-14 13:40:24 +0000

    publishing plot and code

--- week3.R
+++ week3.R
@@ -1,34 +1,37 @@
+# Set Working Directory
+setwd("~/TidyTuesday")
+
 # Load Libraries
-library(tidyverse); library(ggpointdensity); library(ggrepel); library(extrafont)
+library(tidyverse); library(ggrepel); library(extrafont); library(ggthemes)
 
-# Get the Data
-
-
-
-# Value  = Time to crack the password by online guessing
-
-y_crit <- 0.5
-
+# Read in data
 passwords <- na.omit(readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-14/passwords.csv')) %>%
-  dplyr::mutate(category = str_to_title(category)) %>% 
-  mutate(strength = (strength - mean(strength)))
-  
-ggplot(data = passwords)+
-  geom_pointdensity(aes(x = strength, y = log(offline_crack_sec)), shape = 1, size = 3,  adjust = 1)+
-  scale_color_gradient(low = 'steelblue', high = 'darkblue')+
-  geom_label_repel(data = passwords %>% 
-              dplyr::filter(log10(offline_crack_sec) > y_crit), 
-              mapping = (aes(x = strength, y = log(offline_crack_sec), label = password)),
-              size = 3, family = 'Trebuchet MS', seed = 123)+
-  geom_hline(mapping = aes(yintercept = y_crit), linetype = 2.5)+
-  facet_wrap(facets = ~category, nrow = 2)+
-  scale_shape_manual(values=seq(from = 1, to = length(unique(passwords$category)), by = 1))+
-  theme_bw(base_size = 10, base_family = 'Trebuchet MS')+
-  theme(legend.position = 'top')+
-  scale_x_continuous(name = 'Normalised Password Strength')+
-  scale_y_continuous(name = expression(Log[e]('Time Taken to Crack Password Offline', seconds)))+
-  ggtitle(label = 'Strength of Various Categories of Common Passwords', 
-          subtitle = '')+
-  labs(col = 'Data Point Density',
-       caption = '@d73mwf')
-  
+  mutate(category = str_to_title(category)) %>% 
+  mutate(length = nchar(password))
+
+# Plot Parameters
+y_upper <- 0.5; y_lower <- -12; space <- 2; label_size <- 2.75; set.seed(1)
+
+# Creating Plot  
+passwords_plot <- ggplot(data = passwords)+
+  geom_point(aes(x = length, y = log(offline_crack_sec)), shape = 16, size = 1, alpha = 0.2)+
+  geom_text_repel(data = passwords %>% # Add a label for difficult to crack passwords
+              dplyr::filter(log(offline_crack_sec) > y_upper), 
+              mapping = (aes(x = length, y = log(offline_crack_sec), label = password)),
+              size = label_size, family = 'Bahnschrift', segment.size = 0.1, segment.alpha = 0.25,
+              col = 'darkgreen')+
+  geom_text_repel(data = passwords %>% # Add a label for easy to crack passwords
+                     dplyr::filter(log(offline_crack_sec) < y_lower), 
+                   mapping = (aes(x = length, y = log(offline_crack_sec), label = password)),
+                   size = label_size, family = 'Bahnschrift', segment.size = 0.1, segment.alpha = 0.25,
+                   col = 'darkred')+
+  geom_hline(mapping = aes(yintercept = y_upper), linetype = 2, alpha = 0.5, col = 'darkgreen')+ # Draw the threshold for difficult to crack passwords
+  geom_hline(mapping = aes(yintercept = y_lower), linetype = 2, alpha = 0.5, col = 'darkred')+ # Draw the threshold for easy to crack passwords
+  facet_wrap(facets = ~category, nrow = 5)+
+  theme_tufte(base_size = 11, base_family = 'Bahnschrift')+
+  scale_x_continuous(name = 'Password Length', limits = c(min(passwords$length)-space, max(passwords$length) + space))+
+  scale_y_continuous(name = expression(Log[]("Time Taken to Crack Password Offline", seconds)), 
+                     limits = c(min(log(passwords$offline_crack_sec)) - space, max(log(passwords$offline_crack_sec)) + space))+
+  ggtitle(label = "Relationship Between Security and Length of Various Categories of Common Passwords", 
+          subtitle = expression(paste("'Crack Times' of  > 1 second and < ", 10^-12, " seconds are labelled in green and red, respectively")))+
+  labs(caption = "Tidy Tuesday 2020, Week 3. @d73mwf")
