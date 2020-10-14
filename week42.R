8e75b116cb8fecaa69fcbd8050f3c0c4549ef7a7
d73mwf <34099039+DomDF@users.noreply.github.com>
2020-10-14 05:44:39 +0100

    adding week42

---
+++ week42.R
@@ -0,0 +1,68 @@
+datasaurus <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-10-13/datasaurus.csv')
+
+library(tidyverse); library(extrafont)
+
+datasets <- c('dino', 'star')
+
+plot_df <- datasaurus %>% 
+  dplyr::filter(dataset %in% datasets) %>% 
+  group_by(dataset) %>% 
+  mutate(rho = cor(x = x, y = y, method = 'pearson'), 
+         mu_x = mean(x = x), sigma_x = sd(x = x), 
+         mu_y = mean(x = y), sigma_y = sd(x = y)) %>% 
+  ungroup() %>% 
+  mutate(ds_id = as.factor(case_when(
+    dataset == 'dino' ~ 'dataset 1',
+    dataset == 'star' ~ 'dataset 2'
+  )))
+
+boxplot_df <- selected_ds %>% 
+  tidyr::pivot_longer(cols = c(x, y), 
+                      names_to = 'param')
+
+label_df <- boxplot_df %>% 
+  distinct(dataset, .keep_all = TRUE)
+
+summary_stats <- ggplot(data = label_df)+
+  geom_text(mapping = aes(x = 0, y = 0, 
+                          label = paste0('Correlation co-efficent: ', signif(x = rho, digits = 3),
+                                         '\n\n Mean(x): ', signif(x = mu_x, digits = 3), 
+                                         '\n\n Std. Dev.(x): ', signif(x = sigma_x, digits = 3),
+                                         '\n\n Mean(y): ', signif(x = mu_y, digits = 3), 
+                                         '\n\n Std. Dev.(y): ', signif(x = sigma_y, digits = 3))),
+            size = 4, family = 'Bahnschrift')+
+  facet_wrap(facets = ~ ds_id, nrow = 2)+
+  DomDF::theme_ddf_light()+
+  theme(axis.text = element_blank(), 
+        axis.title = element_blank(), 
+        axis.ticks.length = unit(0,'mm'), 
+        strip.text.x = text(color = 'white'))
+
+boxplots <- ggplot(data = selected_ds %>% 
+         tidyr::pivot_longer(cols = c(x, y), 
+                             names_to = 'param'))+
+  geom_boxplot(mapping = aes(y = value ))+
+  facet_grid(ds_id ~ param)+
+  DomDF::theme_ddf_light()+
+  theme(axis.title.y = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.length.x = unit(0,'mm'), 
+        strip.text.y = element_blank())
+
+xy_plot <- ggplot(data = selected_ds, 
+                  mapping = aes(x = x, y = y))+
+  geom_point(alpha = 0.8)+
+  facet_wrap(facets = ~ ds_id, nrow = 2)+
+  DomDF::theme_ddf_light()+
+  theme(axis.ticks.length = unit(0,'mm'),
+        strip.text.x = element_text(color = 'white'))
+
+library(patchwork)
+
+comb_plot <- (summary_stats + boxplots + xy_plot)+
+  plot_annotation(title = 'Plot your data!', 
+                  subtitle = 'Summary statistics may not sufficiently characterise your datasets', 
+                  caption = 'TidyTuesday 2020, Week 42    |    Data from http://www.thefunctionalart.com/    |    @Domenic_DF') &
+  theme(plot.title = element_text(family = 'Bahnschrift'),
+        plot.subtitle = element_text(family = 'Bahnschrift'), 
+        plot.caption = element_text(family = 'Bahnschrift'))
